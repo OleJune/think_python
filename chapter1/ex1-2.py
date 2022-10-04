a154ff49521137f28dc725b8d75c2bdd2d5c6722
Ole June <khmyzok@gmail.com>
2022-10-04 16:45:52 +0300

    chapter1 files

---
+++ chapter1/ex1-2.py
@@ -0,0 +1,8 @@
+sec = 42 * 60 + 42
+print('There are', sec, 'seconds in 42 minutes 42 seconds')
+mile = 10 / 1.61
+print('There are', mile, 'miles in 10 kilometers')
+pace = ((42 * 60 + 42) / (10 / 1.61)) / 60
+print('The average pace is', pace, 'minutes per mile')
+speed = ((10 / 1.61) / (42 * 60 + 42)) * 60**2
+print('The average speed is', speed, 'mph')
