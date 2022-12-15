cac946b166a4cea16007177f99364eb7d0c2c0ca
Ole June <khmyzok@gmail.com>
2022-12-15 17:01:39 +0200

    Extend docstring in ex.5-4

--- chapter5/5-4.py
+++ chapter5/5-4.py
@@ -1,10 +1,9 @@
 def recurse(n, s):
 	"""
-	Calculates and prints the value of argument2 (s) after calling itself 
-	until it reaches the base case where argument1 (n) equals 0.
-
-	# or: The function calls itself until argument1 (n) equals 0
-	#		and then returns the value of argument2 (s).
+	The function calls itself until argument1 (n) equals 0 and then 
+	returns the value of argument2 (s). 
+	And in this case, the value of 's' will be equal to 
+	the sum of the sequence from 'n' to 0, which adds up to the initial value of 's'.
 
 	Parameters:
 		n: positive int
